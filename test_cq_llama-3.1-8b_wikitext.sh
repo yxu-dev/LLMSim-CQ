7fe2f2a72b9d16e4efe53e505e73330845678e68
Thomas Xu <thomas.yifan.xu@outlook.com>
2025-11-24 15:23:52 -0500

    Update path

--- test_cq_llama-3.1-8b_wikitext.sh
+++ test_cq_llama-3.1-8b_wikitext.sh
@@ -1,5 +1,5 @@
 python -m lm_eval.run_models --model hf \
-    --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,cq_codebook_dir=/home/yx277/workspace-vq/LLMSim/fisher_weighted_codebook/llama-3.1-8b/4c8b \
+    --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,cq_codebook_dir=/home/yx277/workspace-vq/fisher_weighted_codebook/llama-3.1-8b/4c8b \
     --tasks wikitext \
     --apply_chat_template \
     --fewshot_as_multiturn \
